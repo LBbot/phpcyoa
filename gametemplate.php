26b1a4d2f5c8f04e4deb868e69d3c3b7b442d5ef
LB <sepherim@hotmail.com>
2018-10-17 11:32:38 +0100

    Add lang attribute to gametemplate

--- gametemplate.php
+++ gametemplate.php
@@ -72,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
